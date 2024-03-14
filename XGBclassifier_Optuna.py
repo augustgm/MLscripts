e2ce67865f3f468caa9a81dcd90eb1622e5cc623
augustgm <91262878+augustgm@users.noreply.github.com>
2024-03-14 11:30:47 +0000

    Update XGBclassifier_Optuna.py

--- XGBclassifier_Optuna.py
+++ XGBclassifier_Optuna.py
@@ -2,6 +2,7 @@
 # Optimise XGBoost for Binary Classification using Optuna #
 ###########################################################
 import pickle
+import joblib
 import optuna
 import numpy as np
 import pandas as pd
@@ -11,12 +12,17 @@ from sklearn.model_selection import RepeatedStratifiedKFold
 from xgboost import XGBClassifier
 from sklearn.metrics import roc_auc_score
 import os
+import gc
 
 # define variables
 num_trials = 100  # number of trials for Optuna optimisation
+train_filename = ""  # training data csv file (assumed to have a column of sample IDs and the target variable)
+hout_filename = ""  # held-out data csv file (assumed to have a column of sample IDs and the target variable)
 input_path = ""  # path to where the train and test sets are stored
 output_path = ""  # path where everything should be saved
 cols_to_scale = []  # columns to scale
+sample_col = ""  # column name containing sample ID
+target_col = ""  # column name containing the target to be predicted (e.g. response status etc.)
 
 
 ######################################################
@@ -47,7 +53,27 @@ def xgboost_objective(trial):
     return XGBClassifier(**hyperparams, random_state=42)
 
 
-base_model = {'model': xgboost_objective}
+def serialise_model(model_obj, model_type, output_path, del_xgb=False):
+    """
+    Save scikit learn or XGBoost sklearn API models
+    :param model_obj: the actual model to be saved
+    :param model_type: string stating what kind of model it is (e.g. "xgboost" or "elastic_net", "random_forest" etc.
+    :param output_path: the path to the location where the model should be saved.
+    Note that the appropriate file ending should not be specified.
+    :param del_xgb: bool specifying whether to delete the XGB model booster and invoke garbage collection or not. Useful
+    when using GPUs to free memory.
+    :return: None
+    """
+    if model_type == "xgboost":
+        # https://mljar.com/blog/xgboost-save-load-python/
+        # https://stackabuse.com/bytes/how-to-save-and-load-xgboost-models/
+        model_obj.save_model(f"{output_path}.json")
+        if del_xgb:
+            model_obj._Booster.__del__()
+            gc.collect()
+    else:  # https://scikit-learn.org/stable/model_persistence.html
+        joblib.dump(model_obj, f"{output_path}.joblib")
+    return None
 
 
 # Define the cross-validation scheme
@@ -57,22 +83,22 @@ cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=10, random_state=42)
 #########################################################
 # Load data, annotate targets, and separate out X and y #
 #########################################################
-X = pd.read_csv(f"{input_path}/train.csv")
-Xtest = pd.read_csv(f"{input_path}/test.csv")
+X = pd.read_csv(f"{input_path}/{train_filename}")
+X_hout = pd.read_csv(f"{input_path}/{hout_filename}")
 
 # Scale shapley values data
 scaler = StandardScaler(with_std=True, with_mean=True)
 
 # Scale the selected columns
 X[cols_to_scale] = scaler.fit_transform(X[cols_to_scale])
-Xtest[cols_to_scale] = scaler.transform(Xtest[cols_to_scale])
+X_hout[cols_to_scale] = scaler.transform(X_hout[cols_to_scale])
 
 # Split x and y
-y = X["targ_stat"]
-X.drop(columns=["targ_stat"], inplace=True)
+y = X[target_col]
+X.drop(columns=[target_col], inplace=True)
 
-ytest = Xtest["targ_stat"]
-Xtest.drop(columns=["targ_stat"], inplace=True)
+y_hout = X_hout[target_col]
+X_hout.drop(columns=[target_col], inplace=True)
 
 del scaler, cols_to_scale
 
@@ -82,10 +108,9 @@ del scaler, cols_to_scale
 ############################
 # Define the objective
 def base_objective(trial):
-    # Define the pipeline for the optuna model with oversampling
-    pipeline = Pipeline(steps=[
-         ('model', base_model['model'](trial))
-    ])
+
+    # Define the model objective
+    model = xgboost_objective(trial)
 
     # Define the scores list
     scores = []
@@ -97,8 +122,8 @@ def base_objective(trial):
         X_test, y_test = X.iloc[test_idx, :], y[test_idx]
 
         # Fit the model and make predictions
-        pipeline.fit(X_train.drop(columns=["gene"]), y_train)
-        y_pred = pipeline.predict_proba(X_test.drop(columns=["gene"]))
+        model.fit(X_train.drop(columns=[sample_col]), y_train)
+        y_pred = model.predict_proba(X_test.drop(columns=[sample_col]))
 
         # Evaluate the predictions
         score = roc_auc_score(y_test, y_pred[:, 1])
@@ -126,7 +151,7 @@ study.optimize(base_objective, n_trials=num_trials)
 print(f"optimised study")
 
 # Save the best parameters to pkl file - Optuna databases to pkl dumps are not readable across different versions
-with open(f"{output_path}/study_xgboost.pkl",'wb') as f:
+with open(f"{output_path}/study_xgboost.pkl", 'wb') as f:
     pickle.dump(study, f)
 print(f"saved study to to .pkl file")
 
@@ -140,39 +165,42 @@ param_dict['objective'] = 'binary:logistic'
 param_dict['eval_metric'] = 'auc'
 param_dict['nthread'] = -1
 
-pipeline = Pipeline([('model', XGBClassifier(**param_dict, random_state=42))])
-
 # Define the predictions list
-pred = pd.DataFrame(columns=['gene', "xgboost"])
-tr_pred = pd.DataFrame(columns=["gene", "xgboost"])
-hout_ts_pred = pd.DataFrame(columns=["gene", "xgboost"])
+tr_pred = pd.DataFrame(columns=[sample_col, "pred_proba"])
+pred = pd.DataFrame(columns=[sample_col, "pred_proba"])
+hout_ts_pred = pd.DataFrame(columns=[sample_col, "pred_proba"])
 
 # Loop over the cross-validation folds and predict with the best parameters
-for train_idx, test_idx in cv.split(X, y):
+for fold_id, train_idx, test_idx in enumerate(cv.split(X, y)):
+    model = XGBClassifier(**param_dict, random_state=42)
 
     # Split the data into train and test sets
     X_train, y_train = X.iloc[train_idx, :], y[train_idx]
     X_test, y_test = X.iloc[test_idx, :], y[test_idx]
 
     # Fit the model and make predictions
-    pipeline.fit(X_train.drop(columns=["gene"]), y_train)
+    model.fit(X_train.drop(columns=[sample_col]), y_train)
 
     # TRAIN set: save predictions to file
-    tr_predictions = pipeline.predict_proba(X_train.drop(columns=["gene"]))[:, 1]
-    tr_pr = pd.DataFrame({"gene": X_train["gene"], "xgboost": tr_predictions})
+    tr_predictions = model.predict_proba(X_train.drop(columns=[sample_col]))[:, 1]
+    tr_pr = pd.DataFrame({sample_col: X_train[sample_col], "CV_fold": fold_id, "pred_proba": tr_predictions})
 
     # TEST set: save the predictions for the stacked ensemble
-    predictions = pipeline.predict_proba(X_test.drop(columns=["gene"]))[:, 1]
-    pr = pd.DataFrame({'gene': X_test["gene"], "xgboost": predictions})
+    predictions = model.predict_proba(X_test.drop(columns=[sample_col]))[:, 1]
+    pr = pd.DataFrame({sample_col: X_test[sample_col], "CV_fold": fold_id, "pred_proba": predictions})
 
     # HELD-OUT TEST set: save predictions to file
-    hout_ts_predictions = pipeline.predict_proba(Xtest.drop(columns=["gene"]))[:, 1]
-    hout_ts_pr = pd.DataFrame({"gene": Xtest["gene"], "xgboost": hout_ts_predictions})
+    hout_ts_predictions = model.predict_proba(X_hout.drop(columns=[sample_col]))[:, 1]
+    hout_ts_pr = pd.DataFrame({sample_col: X_hout[sample_col], "CV_fold": fold_id, "pred_proba": hout_ts_predictions})
 
     # Concatenate to save to file
     tr_pred = pd.concat([tr_pred, tr_pr], axis=0, ignore_index=True)
     pred = pd.concat([pred, pr], axis=0, ignore_index=True)
     hout_ts_pred = pd.concat([hout_ts_pred, hout_ts_pr], axis=0, ignore_index=True)
+
+    # Save trained model to file
+    serialise_model(model_obj=model, model_type="xgboost", del_xgb=False,
+                    output_path=f"{output_path}/xgboost_optimTrainedModel_CVfold{fold_id}.json")
 
 print(f"computed predictions on CV fold")
 
@@ -186,4 +214,3 @@ pred.to_csv(fr"{output_path}/xgboost_testSetCVfolds_predProba.csv", index=False)
 hout_ts_pred.to_csv(fr"{output_path}/xgboost_heldOutTestSetCVfolds_predProba.csv", index=False)
 
 print(f"finished")
-
